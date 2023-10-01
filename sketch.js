4703e2b3521c607f283b856a578964e9ce9d054d
Anish10000 <128833637+Anish10000@users.noreply.github.com>
2023-10-01 10:32:48 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var bgImage
+
+function preload(){
+bgImage=loadImage("assets/bg.jpg")
+}
+
+function setup(){
+ createCanvas(windowWidth,windowHeight)
+ background(bgImage)
+}
+
+
+function draw(){
+
+}
